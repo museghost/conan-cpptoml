6606bd481874003b925d840f212368853f4c5a71
Javier G. Sogo <jgsogo@gmail.com>
2018-01-24 07:52:29 +0100

    change recipe class name

--- conanfile.py
+++ conanfile.py
@@ -5,11 +5,11 @@ from conans import ConanFile, CMake, tools
 import os
 
 
-class LibnameConan(ConanFile):
-    name = "libname"
-    version = "0.0.0"
-    url = "https://github.com/bincrafters/conan-libname"
-    description = "Keep it short"
+class CppTOMLConan(ConanFile):
+    name = "cpptoml"
+    version = "master"  # Last version is v0.4.0 from Feb 2015 (asking for new revision: https://github.com/skystrife/cpptoml/issues/42)
+    url = "https://github.com/bincrafters/conan-cpptoml"
+    description = "cpptoml is a header-only library for parsing TOML "
 
     # Indicates License type of the packaged library
     license = "MIT"
@@ -23,22 +23,16 @@ class LibnameConan(ConanFile):
 
     # Options may need to change depending on the packaged library.
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
+    options = {"build_examples": [True, False]}
+    default_options = ("build_examples=False", )
 
     # Custom attributes for Bincrafters recipe conventions
     source_subfolder = "source_subfolder"
     build_subfolder = "build_subfolder"
 
-    # Use version ranges for dependencies unless there's a reason not to
-    requires = (
-        "OpenSSL/[>=1.0.2l]@conan/stable",
-        "zlib/[>=1.2.11]@conan/stable"
-    )
-
     def source(self):
-        source_url = "https://github.com/libauthor/libname"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
+        source_url = "https://github.com/skystrife/cpptoml"
+        tools.get("{0}/archive/{1}.tar.gz".format(source_url, self.version))
         extracted_dir = self.name + "-" + self.version
 
         #Rename to "source_subfolder" is a convention to simplify later steps
@@ -47,23 +41,10 @@ class LibnameConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False # example
+        cmake.definitions["CPPTOML_BUILD_EXAMPLES"] = self.options.build_examples
         cmake.configure(build_folder=self.build_subfolder)
         cmake.build()
         cmake.install()
-
-    def package(self):
-        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
-        # If so, you can replace all the steps below with the word "pass"
-        include_folder = os.path.join(self.source_subfolder, "include")
-        self.copy(pattern="LICENSE", dst="license", src=self.source_subfolder)
-        self.copy(pattern="*", dst="include", src=include_folder)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
-
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
